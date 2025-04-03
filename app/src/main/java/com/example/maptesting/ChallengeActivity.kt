874bf5ac05fb855107dc5e843df726eb54e6b1ea
DaBigRed <bigred.lundgren@gmail.com>
2025-04-03 10:24:39 -0500

    Fixed photo taking and "uploading"

--- app/src/main/java/com/example/maptesting/ChallengeActivity.kt
+++ app/src/main/java/com/example/maptesting/ChallengeActivity.kt
@@ -1,28 +1,42 @@
 package com.example.maptesting
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.coroutines.launch
-import java.net.URI
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class ChallengeActivity : AppCompatActivity() {
     val firestoreClient = FirestoreClient()
     lateinit var imageView: ImageView
     private lateinit var challenge: Challenge
     private lateinit var user: User
+    private lateinit var uriSave: Uri
+    val REQUEST_IMAGE_CAPTURE = 100
+    lateinit var currentPhotoPath: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,14 +67,19 @@ class ChallengeActivity : AppCompatActivity() {
 
         val photoView = findViewById<Button>(R.id.picture_view)
         photoView.setOnClickListener {
-            val intent = Intent(this, PhotoActivity::class.java)
-            startActivity(intent)
-            finish()
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            //dispatchTakePictureIntent()
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }
         }
 
+
         imageView = findViewById(R.id.pic_display)
+        //imageView.setImageURI(uriSave)
         val pick_photo = findViewById<Button>(R.id.btn_take_picture)
-        var uriSave: Uri? = null
         val pickMedia =
             registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
                 // Callback is invoked after the user selects a media item or closes the
@@ -104,8 +123,7 @@ class ChallengeActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-             // goes back to the previous page
-
+            // goes back to the previous page
 
 
             //Code that gets something from the database
@@ -123,6 +141,121 @@ class ChallengeActivity : AppCompatActivity() {
             }
             */
         }
-
     }
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                val imageBitmap = data?.extras?.get("data") as Bitmap
+                var imageFile = bitmapToFile(null, imageBitmap, "hi.png")
+                imageView.setImageBitmap(imageBitmap)
+                uriSave = imageFile!!.toUri()
+                //if (imageUri != null) {
+                //  uriSave = imageUri
+                //}
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+        private fun createImageFile(): File {
+            // Create an image file name
+            val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            return File.createTempFile(
+                "JPEG_${timeStamp}_", /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+            ).apply {
+                // Save a file: path for use with ACTION_VIEW intents
+                currentPhotoPath = absolutePath
+            }
+        }
+        private fun dispatchTakePictureIntent() {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                // Ensure that there's a camera activity to handle the intent
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    // Create the File where the photo should go
+                    val photoFile: File? = try {
+                        createImageFile()
+                    } catch (ex: IOException) {
+                        // Error occurred while creating the File
+                        null
+                    }
+                    // Continue only if the File was successfully created
+                    photoFile?.also {
+                        val photoURI: Uri = FileProvider.getUriForFile(
+                            this,
+                            "com.example.android.fileprovider",
+                            it
+                        )
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                    }
+                }
+            }
+        }
+        private fun galleryAddPic() {
+            Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+                val f = File(currentPhotoPath)
+                mediaScanIntent.data = Uri.fromFile(f)
+                sendBroadcast(mediaScanIntent)
+            }
+        }
+        private fun setPic() {
+            // Get the dimensions of the View
+            val targetW: Int = imageView.width
+            val targetH: Int = imageView.height
+
+            val bmOptions = BitmapFactory.Options().apply {
+                // Get the dimensions of the bitmap
+                inJustDecodeBounds = true
+
+                BitmapFactory.decodeFile(currentPhotoPath)
+
+                val photoW: Int = outWidth
+                val photoH: Int = outHeight
+
+                // Determine how much to scale down the image
+                val scaleFactor: Int = Math.max(1, Math.min(photoW / targetW, photoH / targetH))
+
+                // Decode the image file into a Bitmap sized to fill the View
+                inJustDecodeBounds = false
+                inSampleSize = scaleFactor
+                inPurgeable = true
+            }
+            BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
+                imageView.setImageBitmap(bitmap)
+            }
+
+        }
+    fun bitmapToFile(
+        context: Context?,
+        bitmap: Bitmap,
+        fileNameToSave: String?
+    ): File? { // File name like "image.png"
+        //create a file to write bitmap data
+        var file: File? = null
+        try {
+            file = File(
+                Environment.getExternalStorageDirectory()
+                    .toString() + File.separator + fileNameToSave
+            )
+            file.createNewFile()
+
+            //Convert bitmap to byte array
+            val bos: ByteArrayOutputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos) // YOU can also save it in JPEG
+            val bitmapdata: ByteArray? = bos.toByteArray()
+
+            //write the bytes in file
+            val fos: FileOutputStream = FileOutputStream(file)
+            fos.write(bitmapdata)
+            fos.flush()
+            fos.close()
+            return file
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return file // it will return null
+        }
+    }
+
 }
+
